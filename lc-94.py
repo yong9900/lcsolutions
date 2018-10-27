90a615ddeb24392c4f5011f4e77413c520c5ead4
Mike Zhao <mzhao.github@gmail.com>
2018-10-26 22:21:42 -0400

    tree first one

---
+++ lc-94.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        stack = []
+        rst  = []
+        stack.append(root)
+        while root.left:
+            stack.append(root.left)
+            root = root.left
+        
+        while stack:
+            node = stack.pop()
+            rst.append(node.val)
+            if node.right:
+                node=node.right
+                stack.append(node)
+                if node.left:
+                    stack.append(node.left)
+                    node=node.left
+       
+        return rst
