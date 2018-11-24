5c4ed7410cf521050109d579ad8304a47b6be07d
yong9900 <yong.9900@gmail.com>
2018-11-24 10:40:21 -0500

    s3 draft

--- ccc/2018/2018s3.py
+++ ccc/2018/2018s3.py
@@ -8,18 +8,26 @@ for file in filelist:
         r,c=map(int, f.readline().strip().split())
         a = [] # the map
         c = [] # "C" position
-
-
-        ans = [[-1]*c for i in range(r)]
+        ans = [[-1]*c for i in range(r)] # steps
         #read in
         for i in range(r):
            b = [j for j in f.readline().strip()]
            for j in range(c):
                if b[j]=='S':
-                   srow,scol=i,j
-                   ans[srow][scol]=0
+                    srow,scol=i,j
+                    ans[srow][scol]=0
+                if b[j]='C':
+                    c.append((i,j))
            a.append(b)
         #handle C case
+        change_to_W = []
+        dir=[(1,0), (-1,0), (0,1), (0,-1)]
+        for i,j in c:
+            ridx, cidx = i,j
+            for rinc, cinc in dir:
+                while a[ridx][cidx] != 'W' and ridx>=0 and cidx>0 and ridx<r and cidx<c:
+                    if a[ridx][cidx] == '.':
+                        change_to_W.append((ridx,cidx))
 
         for i in range(r):
             for j in range(c):
