848577cbda919ec86445cabc1bc6d3130c26f6f6
mike zhao <yong.9900@gmail.com>
2018-11-25 15:45:13 -0500

    correct

--- ccc/2018/2018s3.py
+++ ccc/2018/2018s3.py
@@ -5,9 +5,10 @@ filelist = glob.glob('D:/download/windows_data/S3/*.in')
 for file in filelist:
     with open(file) as f:
         t1=time.time()
-        r,c=map(int, f.readline().strip().split())
+        r,c =map(int, f.readline().strip().split())
         a = [] # the map
-        c = [] # "C" position
+        c_pos = [] # "C" position
+        dot_pos = []
         ans = [[-1]*c for i in range(r)] # steps
         #read in
         for i in range(r):
@@ -15,70 +16,78 @@ for file in filelist:
            for j in range(c):
                 if b[j]=='S':
                     srow,scol=i,j
-                    ans[srow][scol]=0
-                if b[j]='C':
-                    c.append((i,j))
+                if b[j]=='C':
+                    c_pos.append((i,j))
+                if b[j]=='.':
+                    dot_pos.append((i,j))
            a.append(b)
+
         #handle C case
-        change_to_W = []
-        
         def checkpoint(row, col):
             if row>=0 and row<r and col>=0 and col<c:
                 return True
             return False
-
+        change_to_W = []
+        valid_start = True
         cov=['D','U','R','L'] 
         dir=[(1,0), (-1,0), (0,1), (0,-1)]
-        for i,j in c:
-            ridx, cidx = i,j
+        for ridx,cidx in c_pos:
             for rinc, cinc in dir:
-                while a[ridx][cidx] != 'W' and checkpoint(ridx,cidx):
-                    if a[ridx][cidx] == '.':
-                        change_to_W.append((ridx,cidx))
-                    ridx += rinc
-                    cinc += cinc
+                i,j = ridx, cidx
+                while a[i][j] != 'W' and checkpoint(i,j):
+                    if i==srow and j==scol:
+                        valid_start = False
+                        break
+                    if a[i][j] == '.':
+                        change_to_W.append((i,j))
+                    i,j = i + rinc, j + cinc
+                if not valid_start:
+                    break
+            if not valid_start:
+                break
         for pointr, pointc in change_to_W:
             a[pointr][pointc] = 'W'
-        #BFS
-        queue=[(srow, scol)]
-        new_queue=[]
-        step=2
-        ans[srow][scol]=1
-        while queue:
-            for pr,pc in queue:
-                for rinc, cinc in dir:
-                    cur_row= pr+rinc
-                    cur_col= pc+cinc
-                    if checkpoint(cur_row, cur_col):
-                        if a[cur_row][cur_col] == '.':
-                            new_queue.append((cur_row, cur_col))
-                            ans[cur_row][cur_col]=step
-                        while a[cur_row][cur_col] in cov:
-                            cur_row += dir[cov.index(a[cur_row][cur_col])][0]
-                            cur_col += dir[cov.index(a[cur_row][cur_col])][1]
-                            if not checkpoint(cur_row, cur_col):
-                                break
-                            if ans[cur_row][cur_col] not in cov:
-                                if a[cur_row][cur_col] == '.':
+
+        if valid_start and (srow, scol) not in change_to_W:
+            #BFS
+            queue=[(srow, scol)]
+            new_queue=[]
+            step=1
+            ans[srow][scol]=1
+            while queue:
+                for pr,pc in queue:
+                    for rinc, cinc in dir:
+                        cur_row= pr+rinc
+                        cur_col= pc+cinc
+                        if checkpoint(cur_row, cur_col):
+                            in_loop =[]
+                            while a[cur_row][cur_col] in cov:
+                                if (cur_row, cur_col) in in_loop:
+                                    break
+                                in_loop.append((cur_row, cur_col))
+                                row_inc, col_inc = dir[cov.index(a[cur_row][cur_col])]
+                                cur_row, cur_col = cur_row+row_inc, cur_col+col_inc
+                                if not checkpoint(cur_row, cur_col):
+                                    break
+                            else:
+                                if a[cur_row][cur_col] == '.' and ans[cur_row][cur_col] < 0:
                                     new_queue.append((cur_row, cur_col))
                                     ans[cur_row][cur_col]=step
-                                break
-            queue=new_queue
-            new_queue=[]
+                queue=new_queue
+                new_queue = []
+                step += 1
         #print result
-                duration = time.time()-t1
+        duration = time.time()-t1
         ofile = file.replace('.in', '.out')
         exp = ''
         with open(ofile) as of:
             correct = True
-            for i in range(r):
-                for j in range(c):
-                    if a[i][j] == '.':
-                        exp=int(of.readline().strip())
-                        if ans[i][j] != exp:
-                            print("wrong for " + file + "in position " + str(i) + "," + str(j) + ". exp:" + str(exp) + ",res:" + str(ans))
-                            correct = False
-                            break
+            for i,j in dot_pos:
+                exp=int(of.readline().strip())
+                if ans[i][j] != exp:
+                    print("wrong for " + file + "in position " + str(i) + "," + str(j) + ". exp:" + str(exp) + ",res:" + str(ans))
+                    correct = False
+                    break
                 if not correct:
                     break
             print("take {} seconds".format(duration))
